47b667a124f909846bad628594f17bc359854175
Matt Howlett <matt.howlett@gmail.com>
2022-12-23 08:17:42 +0700

    connect item on add as well as load

--- infumap/src/store/item_store.rs
+++ infumap/src/store/item_store.rs
@@ -50,53 +50,7 @@ impl ItemStore {
     }
 
     let store: KVStore<Item> = KVStore::init(&self.data_dir, &log_filename)?;
-
-    for (_id, item) in store.get_iter() {
-      match &item.parent_id {
-        Some(parent_id) => {
-          match item.relationship_to_parent {
-            RelationshipToParent::Child => {
-              match self.children_of.get_mut(parent_id) {
-                Some(children) => {
-                  children.push(item.id.clone());
-                },
-                None => {
-                  self.children_of.insert(parent_id.clone(), vec![item.id.clone()]);
-                }
-              }
-            },
-            RelationshipToParent::Attachment => {
-              match self.attachments_of.get_mut(parent_id) {
-                Some(attachments) => {
-                  attachments.push(item.id.clone());
-                },
-                None => {
-                  self.attachments_of.insert(parent_id.clone(), vec![item.id.clone()]);
-                }
-              }
-            },
-            RelationshipToParent::NoParent => {
-              return Err(InfuError::new("NoParent relationship is invalid except for root element."));
-            }
-          }
-        },
-        None => {
-          if item.relationship_to_parent != RelationshipToParent::NoParent {
-            return Err(InfuError::new("Root element relationship to parent is not NoParent."));
-          }
-          // By convention, root level items are children of themselves.
-          match self.children_of.get_mut(&item.id) {
-            Some(children) => {
-              children.push(item.id.clone());
-            },
-            None => {
-              self.children_of.insert(item.id.clone(), vec![item.id.clone()]);
-            }
-          }
-        }
-      }
-    }
-
+    for (_id, item) in store.get_iter() { self.connect_item(item)?; }
     self.store_by_user_id.insert(String::from(user_id), store);
 
     Ok(())
@@ -106,9 +60,46 @@ impl ItemStore {
     todo!()
   }
 
+  fn connect_item(&mut self, item: &Item) -> InfuResult<()> {
+    match &item.parent_id {
+      Some(parent_id) => {
+        match item.relationship_to_parent {
+          RelationshipToParent::Child => {
+            match self.children_of.get_mut(parent_id) {
+              Some(children) => { children.push(item.id.clone()); },
+              None => { self.children_of.insert(parent_id.clone(), vec![item.id.clone()]); }
+            }
+          },
+          RelationshipToParent::Attachment => {
+            match self.attachments_of.get_mut(parent_id) {
+              Some(attachments) => { attachments.push(item.id.clone()); },
+              None => { self.attachments_of.insert(parent_id.clone(), vec![item.id.clone()]); }
+            }
+          },
+          RelationshipToParent::NoParent => {
+            return Err(InfuError::new("NoParent relationship is invalid except for root element."));
+          }
+        }
+      },
+      None => {
+        if item.relationship_to_parent != RelationshipToParent::NoParent {
+          return Err(InfuError::new("Root element relationship to parent is not NoParent."));
+        }
+        // By convention, root level items are children of themselves.
+        match self.children_of.get_mut(&item.id) {
+          Some(children) => { children.push(item.id.clone()); },
+          None => { self.children_of.insert(item.id.clone(), vec![item.id.clone()]); }
+        }
+      }
+    }
+
+    Ok(())
+  }
+
   pub fn add(&mut self, item: Item) -> InfuResult<()> {
     let store = self.store_by_user_id.get_mut(&item.owner_id)
       .ok_or(InfuError::new(&format!("Store has not been loaded for user '{}'", item.owner_id)))?;
-    store.add(item)
+    store.add(item.clone())?;
+    self.connect_item(&item)
   }
 }
