a907e4fd9783175ee63d993747acca9bc0243812
Matt Howlett <matt.howlett@gmail.com>
2023-01-08 17:13:45 +0700

    no more solid-js stores

--- web/src/store/ItemStoreProvider.tsx
+++ web/src/store/ItemStoreProvider.tsx
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2022 Matt Howlett
+  Copyright (C) 2022-2023 Matt Howlett
   This file is part of Infumap.
 
   This program is free software: you can redistribute it and/or modify
@@ -16,24 +16,16 @@
   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-import { batch, JSX } from "solid-js";
+import { Accessor, batch, createSignal, JSX, Setter } from "solid-js";
 import { createContext, useContext } from "solid-js";
-import { createStore, produce } from "solid-js/store";
 import { panic, throwExpression } from "../util/lang";
 import { asPageItem } from "./items/page-item";
-import { Item, setFromParentId } from "./items/base/item";
+import { cloneItem, Item } from "./items/base/item";
 import { Uid } from "../util/uid";
 import { Child, NoParent } from "../relationship-to-parent";
 import { asContainerItem, ContainerItem, isContainerItem } from "./items/base/container-item";
 import { newOrderingAtEnd } from "../util/ordering";
 
-
-export type Items = {
-  rootId: Uid | null,
-  fixed: { [id: Uid]: Item },
-  moving: Array<Item>
-  // Also need some way to keep track of parent pages that haven't been loaded yet.
-}
 
 export interface ItemStoreContextModel {
   setRoot: (id: Uid) => void,
@@ -56,39 +48,48 @@ export interface ItemStoreContextProps {
 
 const ItemStoreContext = createContext<ItemStoreContextModel>();
 
-export function ItemStoreProvider(props: ItemStoreContextProps) {
-  const [items, setItems] = createStore<Items>({
-    rootId: null,
-    fixed: {},
-    moving: []
-  });
+interface ItemSignal {
+  item: Accessor<Item>,
+  setItem: Setter<Item>,
+}
 
-  const setRoot = (id: Uid): void => { setItems("rootId", id); };
+function createItemSignal(item: Item): ItemSignal {
+  let [itemAccessor, itemSetter] = createSignal<Item>(item);
+  return { item: itemAccessor, setItem: itemSetter };
+}
+
+export function ItemStoreProvider(props: ItemStoreContextProps) {
+  const [_rootId, setRootId] = createSignal<Uid | null>(null);
+  let moving: Array<ItemSignal> = [];
+  let fixed: { [id: Uid]: ItemSignal } = {};
+  // Also need some way to keep track of parent pages that haven't been loaded yet.
+
+  const setRoot = (id: Uid): void => { setRootId(id); };
 
   const updateItem = (id: Uid, f: (item: Item) => void): void => {
-    if (items.fixed.hasOwnProperty(id)) {
-      setItems("fixed", produce(items => {
-        f(items[id]);
-      }));
+    if (fixed.hasOwnProperty(id)) {
+      let clonedItem = cloneItem(fixed[id].item());
+      f(clonedItem);
+      fixed[id].setItem(clonedItem);
     } else {
-      setItems("moving", produce(items => {
-        for (let i=0; i<items.length; ++i) {
-          if (items[i].id == id) {
-            f(items[i]);
-            break;
-          }
+      for (let i=0; i<moving.length; ++i) {
+        if (moving[i].item().id == id) {
+          let clonedItem = cloneItem(moving[i].item());
+          f(clonedItem);
+          moving[i].setItem(clonedItem);
+          break;
         }
-      }));
+      }
     }
   };
 
   const getItem = (id: Uid): Item | null => {
-    if (items.fixed.hasOwnProperty(id)) {
-      return items.fixed[id];
+    if (fixed.hasOwnProperty(id)) {
+      return fixed[id].item();
     } else {
-      for (let i=0; i<items.moving.length; ++i) {
-        if (items.moving[i].id == id) {
-          return items.moving[i];
+      for (let i=0; i<moving.length; ++i) {
+        if (moving[i].item().id == id) {
+          return moving[i].item();
         }
       }
     }
@@ -96,42 +97,49 @@ export function ItemStoreProvider(props: ItemStoreContextProps) {
   };
 
   const getFixedItem = (id: Uid): Item | null => {
-    if (items.fixed.hasOwnProperty(id)) {
-      return items.fixed[id];
+    if (fixed.hasOwnProperty(id)) {
+      return fixed[id].item();
     }
     return null;
   };
 
   const getMovingItems = (): Array<Item> => {
-    return items.moving;
+    return moving.map(i => i.item());
   }
 
   const transitionToMove = (id: Uid): void => {
-    setItems(produce(items => {
-      let item = items.fixed[id];
-      delete items.fixed[id];
-      asPageItem(items.fixed[item.parentId ?? panic()]).computed_children = asPageItem(items.fixed[item.parentId ?? panic()]).computed_children.filter(itm => itm != id);
-      setFromParentId(item, item.parentId ?? panic());
-      items.moving.push(item);
-    }));
+    let itemSignal = fixed[id];
+    delete fixed[id];
+    let itemClone = cloneItem(itemSignal.item());
+    itemClone.computed_fromParentIdMaybe = itemClone.parentId;
+    itemSignal.setItem(itemClone);
+    moving.push(itemSignal);
+
+    let parent = asPageItem(cloneItem(fixed[itemSignal.item().parentId ?? panic()].item()));
+    parent.computed_children = parent.computed_children.filter(item => item != id);
+    fixed[parent.id].setItem(parent);
   };
 
   const transitionMovingToFixed = (): void => {
-    setItems(produce(items => {
-      let movingItems = items.moving;
-      items.moving = [];
-      movingItems.forEach(item => {
-        items.fixed[item.id] = item;
-        asPageItem(items.fixed[item.parentId ?? panic()]).computed_children.push(item.id);
-      });
-    }));
+    moving.forEach(itemSignal => {
+      let cloned = cloneItem(itemSignal.item());
+      cloned.computed_fromParentIdMaybe = null
+      itemSignal.setItem(cloned);
+      fixed[itemSignal.item().id] = itemSignal;
+
+      let parentId = itemSignal.item().parentId ?? panic();
+      let parent = asPageItem(cloneItem(fixed[parentId].item()));
+      parent.computed_children.push(itemSignal.item().id);
+      fixed[parentId].setItem(parent);
+    });
+    moving = [];
   };
 
   // Note: the items array contains the container item itself, in addition to the children, if the container is the root.
   const setChildItems = (parentId: Uid, childItems: Array<Item>): void => {
     batch(() => {
       childItems.forEach(childItem => {
-        setItems("fixed", produce(itms => { itms[childItem.id] = childItem; }));
+        fixed[childItem.id] = createItemSignal(childItem);
       });
       childItems.forEach(childItem => {
         if (childItem.parentId == null) {
@@ -154,27 +162,27 @@ export function ItemStoreProvider(props: ItemStoreContextProps) {
   }
 
   const addItem = (item: Item): void => {
-    setItems("fixed", produce(items => { items[item.id] = item; }));
-    if (item.relationshipToParent == Child) {
-      updateItem(item.parentId!, parentItem => {
-        if (!isContainerItem(parentItem)) { panic(); }
-        (parentItem as ContainerItem).computed_children = [...(parentItem as ContainerItem).computed_children, item.id];
-      })
-    } else {
-      throwExpression("only support child relationships currently");
-    }
+    batch(() => {
+      fixed[item.id] = createItemSignal(item);
+      if (item.relationshipToParent == Child) {
+        updateItem(item.parentId!, parentItem => {
+          if (!isContainerItem(parentItem)) { panic(); }
+          (parentItem as ContainerItem).computed_children = [...(parentItem as ContainerItem).computed_children, item.id];
+        })
+      } else {
+        throwExpression("only support child relationships currently");
+      }
+    });
   }
 
   const replaceWithClone = (currentPageId: Uid): void => {
-    setItems("fixed", produce(items => {
-      items[currentPageId] = { ...items[currentPageId] };
-      return items;
-    }));
+    let itm = cloneItem(fixed[currentPageId].item());
+    fixed[currentPageId].setItem(itm);
   };
 
   const newOrderingAtEndOfChildren = (parentId: Uid): Uint8Array => {
-    let parent = asContainerItem(items.fixed[parentId]);
-    let children = parent.computed_children.map(c => items.fixed[c].ordering);
+    let parent = asContainerItem(fixed[parentId].item());
+    let children = parent.computed_children.map(c => fixed[c].item().ordering);
     return newOrderingAtEnd(children);
   }
 
